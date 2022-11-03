bc854dfefa7c1e2955f8d05c30f222f45fc72149
Basu Singh <basu.singh@hypd.in>
2022-11-04 00:15:01 +0530

    sign up page designing

--- app/src/main/java/com/example/gamesetmatch/MainActivity.kt
+++ app/src/main/java/com/example/gamesetmatch/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.gamesetmatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.gamesetmatch.databinding.ActivityMainBinding
+import com.example.gamesetmatch.signupsignin.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.getStartedBtn.setOnClickListener {
+            Intent(this, SignUpActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/gamesetmatch/signupsignin/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gamesetmatch.signupsignin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gamesetmatch.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
