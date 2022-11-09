c782d77069f483818da46b278adf50130daeb382
Basu Singh <basu.singh@hypd.in>
2022-11-10 02:01:16 +0530

    bottom sheet opens when add player is clicked

--- app/src/main/java/com/example/gamesetmatch/ui/HomePageActivity.kt
+++ app/src/main/java/com/example/gamesetmatch/ui/HomePageActivity.kt
@@ -1,10 +1,13 @@
 package com.example.gamesetmatch.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
 import com.example.gamesetmatch.R
 import com.example.gamesetmatch.databinding.ActivityHomePageBinding
+import com.example.gamesetmatch.ui.addplayer.AddPlayerActivity
 import com.example.gamesetmatch.util.Constants
 
 class HomePageActivity : AppCompatActivity() {
@@ -24,6 +27,12 @@ class HomePageActivity : AppCompatActivity() {
         if(username != null){
             val welcomeMessage = "Hello, $username!"
             binding.welcomeMessageTv.text = welcomeMessage
+        }
+
+        binding.addPlayersBtn.setOnClickListener {
+            Intent(this, AddPlayerActivity::class.java).also {
+                startActivity(it)
+            }
         }
     }
 }
--- app/src/main/java/com/example/gamesetmatch/ui/addplayer/AddPlayerActivity.kt
+++ app/src/main/java/com/example/gamesetmatch/ui/addplayer/AddPlayerActivity.kt
@@ -2,11 +2,70 @@ package com.example.gamesetmatch.ui.addplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
 import com.example.gamesetmatch.R
+import com.example.gamesetmatch.databinding.ActivityAddPlayerBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class AddPlayerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddPlayerBinding
+    private lateinit var addPlayerBottomSheetDialog: BottomSheetDialog
+
+
+    private lateinit var playerNameET: EditText
+    private lateinit var optionFootballTV: TextView
+    private lateinit var optionBasketballTV: TextView
+    private lateinit var footballGradeA: TextView
+    private lateinit var footballGradeB: TextView
+    private lateinit var footballGradeC: TextView
+    private lateinit var basketballGradeA: TextView
+    private lateinit var basketballGradeB: TextView
+    private lateinit var basketballGradeC: TextView
+    private lateinit var doneBtn: AppCompatButton
+    private lateinit var cancelBtn: AppCompatButton
+    private lateinit var closeBtn: ImageView
+    private lateinit var footballGradesLayout: ConstraintLayout
+    private lateinit var basketballGradesLayout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_player)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_player)
+        initiateAddPlayerBottomSheet()
+
+        binding.addPlayersBtn.setOnClickListener {
+            addPlayerBottomSheetDialog.show()
+        }
+    }
+
+    private fun initiateAddPlayerBottomSheet(){
+        val addPlayerBottomSheet = LayoutInflater.from(this).inflate(R.layout.bottomsheet_add_player, null)
+        addPlayerBottomSheetDialog = BottomSheetDialog(this, R.style.BottomSheetDialogTheme)
+        addPlayerBottomSheetDialog.setContentView(addPlayerBottomSheet)
+
+        playerNameET = addPlayerBottomSheet.findViewById(R.id.player_name_et)
+        optionFootballTV = addPlayerBottomSheet.findViewById(R.id.option_football)
+        optionBasketballTV = addPlayerBottomSheet.findViewById(R.id.option_basketball)
+        footballGradeA = addPlayerBottomSheet.findViewById(R.id.football_grade_a)
+        footballGradeB = addPlayerBottomSheet.findViewById(R.id.football_grade_b)
+        footballGradeC = addPlayerBottomSheet.findViewById(R.id.football_grade_c)
+        basketballGradeA = addPlayerBottomSheet.findViewById(R.id.basketball_grade_a)
+        basketballGradeB = addPlayerBottomSheet.findViewById(R.id.basketball_grade_b)
+        basketballGradeC = addPlayerBottomSheet.findViewById(R.id.basketball_grade_c)
+        doneBtn = addPlayerBottomSheet.findViewById(R.id.done_btn)
+        cancelBtn = addPlayerBottomSheet.findViewById(R.id.cancel_button)
+        closeBtn = addPlayerBottomSheet.findViewById(R.id.close_btn)
+        footballGradesLayout = addPlayerBottomSheet.findViewById(R.id.football_grades)
+        basketballGradesLayout = addPlayerBottomSheet.findViewById(R.id.basketball_grades)
+
+        closeBtn.setOnClickListener {
+            addPlayerBottomSheetDialog.dismiss()
+        }
     }
 }
