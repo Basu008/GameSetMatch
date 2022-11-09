59da02e902702a1d6e78d0fa22a6479b755f77f2
Basu Singh <basu.singh@hypd.in>
2022-11-10 00:25:28 +0530

    player item designed

---
+++ app/src/main/java/com/example/gamesetmatch/ui/addplayer/AddPlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gamesetmatch.ui.addplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gamesetmatch.R
+
+class AddPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_player)
+    }
+}
